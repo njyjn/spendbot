a0fcc214f4b38dc41cd0000c161d1209b68b02e0
Justin Ng <justin@aletheon.co>
2023-04-29 15:09:55 +0800

    Add responsive collapsing of navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,14 @@
 import Navigation from "./navigation";
 import Footer from "./footer";
 import { PropsWithChildren } from "react";
+import { Container } from "react-bootstrap";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <>
+    <Container fluid>
       <Navigation />
       <main>{children}</main>
       <Footer />
-    </>
+    </Container>
   );
 }
